292c80d09a6ede8d898808dac917cffaa1536501
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2017-09-20 23:30:09 +0200

    added team selection

--- src/components/player-list/player-list.js
+++ src/components/player-list/player-list.js
@@ -1,13 +1,18 @@
 import tableTemplate from './table.html'
 import rowTemplate from './row.html'
+import teamSelection from '../team-selection/team-selection'
 
 import { htmlToElement } from '../../dom-utils'
 
-const renderRow = player => {
+const renderRow = (player, teams) => {
   const row = htmlToElement(rowTemplate)
 
   row.querySelector('[role=row-name]').innerText = player.name
   row.querySelector('[role=row-email]').innerText = player.email
+
+  const teamSelectionElement = teamSelection.render(teams)
+
+  row.querySelector('team-selection').appendChild(teamSelectionElement)
 
   return row
 }
@@ -17,7 +22,11 @@ const render = data => {
 
   const tBody = table.querySelector('tbody')
 
-  data.players.forEach(player => tBody.appendChild(renderRow(player)))
+  const { players, teams } = data
+
+  data.players.forEach(player => {
+    tBody.appendChild(renderRow(player, teams))
+  })
 
   return table
 }
---
+++ src/components/team-selection/team-selection.js
@@ -0,0 +1,23 @@
+import template from './team-selection.html'
+
+import { htmlToElement } from '../../dom-utils'
+
+const render = (teams, onChangeTeam) => {
+  const teamSelectionElement = htmlToElement(template)
+
+  const select = teamSelectionElement.querySelector('select')
+
+  teams.forEach(team => {
+    const option = document.createElement('option')
+    option.text = team.name
+    option.value = team.id
+
+    select.add(option)
+  })
+
+  return teamSelectionElement
+}
+
+export default {
+  render
+}
