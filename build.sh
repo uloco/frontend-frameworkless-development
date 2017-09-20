d7b2b3b1b21e3987c81153b7b875c8af92aa1e9d
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2017-09-20 22:34:37 +0200

    fixed build script

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash    
-cp $(npm root)/public-project-stub/_variables.scss $(npm root)/sierra-library/src/
-cp $(npm root)/public-project-stub/_variables.light.scss $(npm root)/sierra-library/src/
-cd $(npm root)/sierra-library/ 
+cp node_modules/public-project-stub/_variables.scss node_modules/sierra-library/src/
+cp node_modules/public-project-stub/_variables.light.scss node_modules/sierra-library/src/
+cd node_modules/sierra-library/ 
 npm install 
-gulp build 
+./node_modules/.bin/gulp build 
 mv dist/sierra.min.css ../../extrategy-styleguide.css
