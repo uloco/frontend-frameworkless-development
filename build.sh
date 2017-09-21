7bb2a0a89d3a33179ba0f6d1bd05bcd9a6210fef
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2017-09-21 20:45:26 +0200

    removed styleguide dep

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash    
-cp node_modules/public-project-stub/_variables.scss node_modules/sierra-library/src/
-cp node_modules/public-project-stub/_variables.light.scss node_modules/sierra-library/src/
-cd node_modules/sierra-library/ 
-npm install 
-./node_modules/.bin/gulp build 
-mv dist/sierra.min.css ../../extrategy-styleguide.css
